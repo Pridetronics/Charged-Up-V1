948c3d6ed66beb900b1d79721a6282ddcd4436b4
Matthew Brown <matthew.brown@pridetronics.com>
2023-08-15 15:24:57 -0700

    first commit

--- Documents/Swerve-Drive/Swerve-Drive/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ Documents/Swerve-Drive/Swerve-Drive/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,13 +1,13 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.AnalogInput;
-import com.revrobotics.CANAnalog;
-import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxAnalogSensor;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.AnalogInput;
 
 public class SwerveModule {
     private final CANSparkMax driveMotor;
@@ -18,17 +18,27 @@ public class SwerveModule {
 
     private final PIDController  turnignPIDController;
 
-    private SparkMaxAnalogSensor absoluteEncoder;
+    private final SparkMaxAnalogSensor absoluteEncoder;
     private final boolean absoluteEncoderReversed;
     private final double absoluteEncoderOffsetRad;
 
     public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, 
     boolean turningMotorReversed, int absoluteEncoderId, double abolsoluteEncoderOffset, boolean absoluteEncoderReversed) {
 
+        driveMotor = new CANSparkMax(absoluteEncoderId, MotorType.kBrushless);
+        turningMotor = new CANSparkMax(turningMotorId, MotorType.kBrushless);
+
+        driveMotor.setInverted(driveMotorReversed);
+        turningMotor.setInverted(turningMotorReversed);
+
         this.absoluteEncoderOffsetRad = abolsoluteEncoderOffset;
         this.absoluteEncoderReversed = absoluteEncoderReversed;
         absoluteEncoder = turningMotor.getAnalog(SparkMaxAnalogSensor.Mode.kAbsolute);
+        //absoluteEncoder = new AnalogInput(absoluteEncoderId);
 
+        drivEncoder = driveMotor.getEncoder();
+        turningEncoder = turningMotor.getEncoder();
 
+        drivEncoder.set
     }
 }
